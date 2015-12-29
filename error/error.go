9ffe820bd41fd687dd19fcb4a286d1c2255e6865
Quentin Renard <qrenard@molotov.tv>
2015-12-29 14:46:53 +0100

    Added parseError

--- error/error.go
+++ error/error.go
@@ -1,5 +1,10 @@
 package error
 
+import (
+	"fmt"
+	"reflect"
+)
+
 func ProcessError(oErr interface{}) {
 	if oErr != nil {
 		panic(oErr)
@@ -9,5 +14,19 @@ func ProcessError(oErr interface{}) {
 func Catch(fCallback func(oErr interface{}, aArgs map[string]interface{}), aArgs map[string]interface{}) {
 	if e := recover(); e != nil {
 		fCallback(e, aArgs)
+	}
+}
+
+func ParseError(e interface{}) string {
+	if _, ok := e.(error); ok {
+		return e.(error).Error()
+	} else if _, ok := e.(string); ok {
+		return e.(string)
+	} else {
+		return fmt.Sprintf(
+			"Unparseable error message for type <%s> and error <%s>",
+			reflect.TypeOf(e).String(),
+			fmt.Sprint(e),
+		)
 	}
 }
